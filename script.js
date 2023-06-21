2b94e7bbc8b04ed5cc69217425e42afb72402bea
khadija aslam <iamkadijahaslam0@gmail.com>
2023-06-21 12:27:39 +0500

    implementing page navigation

--- script.js
+++ script.js
@@ -57,3 +57,11 @@ btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
+// IMPLEMENTING PAGE NAVIGATION
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
